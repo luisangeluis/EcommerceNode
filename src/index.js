d531ab6af95a3bf5c39b9cf3ff7b1df5a222d6eb
luisangeluis <luisangeluis@gmail.com>
2023-01-27 16:36:50 -0600

    Added PORT variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 const {app} = require('./app');
 
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`);
