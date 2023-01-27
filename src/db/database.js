e4b5b93fa282d35ff052c9e469e992015163df73
luisangeluis <luisangeluis@gmail.com>
2023-01-27 17:16:52 -0600

    Added bd port

--- src/db/database.js
+++ src/db/database.js
@@ -6,7 +6,8 @@ const db = new Sequelize({
   host: process.env.HOST,
   username: process.env.USER,
   password:process.env.PASSWORD,
-  database:process.env.DB
+  database:process.env.DB,
+  port:process.env.DB_PORT || "3306"
 })
 
 module.exports={
