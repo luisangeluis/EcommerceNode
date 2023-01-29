b400d9d6504c0d1d9748f35e8a141f1866fa026c
luisangeluis <luisangeluis@gmail.com>
2023-01-28 21:38:29 -0600

    Commented controllers

--- src/controllers/companies.controllers.js
+++ src/controllers/companies.controllers.js
@@ -4,14 +4,18 @@ const uuid = require('uuid');
 const Companies = require('../models/company.model');
 const CompanyTypes = require('../models/companyType.model');
 
+/**
+ * Gets all companies where isActive equals true
+ * @returns {Object} Return all companies
+ */
 const readAllCompanies = async () => {
   const response = await Companies.findAll({
     where: { isActive: true },
-    attributes: ['id', 'name', 'constitutionDate', 'constitutionDate','comments','isActive'],
-    include:[
+    attributes: ['id', 'name', 'constitutionDate', 'constitutionDate', 'comments', 'isActive'],
+    include: [
       {
-        model:CompanyTypes,
-        attributes:['id','name']
+        model: CompanyTypes,
+        attributes: ['id', 'name']
       }
     ]
   });
@@ -19,16 +23,26 @@ const readAllCompanies = async () => {
   return response;
 }
 
+/**
+ * Get company by id
+ * @param {Integer} id Gets the company id 
+ * @returns {Object} Returns the company found
+ */
 const readCompanyById = async (id) => {
   const response = await Companies.findOne({
     where: { id },
-    attributes: ['id', 'name', 'constitutionDate','typeId', 'constitutionDate','comments','isActive']
-    
+    attributes: ['id', 'name', 'constitutionDate', 'typeId', 'constitutionDate', 'comments', 'isActive']
+
   })
 
   return response;
 }
 
+/**
+ * Create a company
+ * @param {Object} data Gets data to create a company
+ * @returns {Object} returns the company created
+ */
 const createCompany = async (data) => {
   const response = await Companies.create({
     ...data,
@@ -39,16 +53,27 @@ const createCompany = async (data) => {
   return response;
 }
 
+/**
+ * Update a company
+ * @param {Integer} companyId Get the company id
+ * @param {Object} data Gets data to update a company
+ * @returns {Object}Return response
+ */
 const updateCompany = async (companyId, data) => {
-  const {id,...restOfData} = data;
+  const { id, ...restOfData } = data;
   const response = await Companies.update(
     restOfData,
-    { where: { id:companyId } }
+    { where: { id: companyId } }
   )
 
   return response;
 }
 
+/**
+ * Delete a company
+ * @param {Integer} id Gets the company id 
+ * @returns {Object}return response
+ */
 const deleteCompany = async (id) => {
   const response = await Companies.destroy({
     where: { id }
--- src/controllers/companyTypes.controller.js
+++ src/controllers/companyTypes.controller.js
@@ -4,6 +4,10 @@ const uuid = require('uuid');
 //Models
 const CompanyTypes = require('../models/companyType.model');
 
+/**
+ * Get all company types
+ * @returns {Object} Return all company types
+ */
 const readAllCompanyTypes = async () => {
   const response = await CompanyTypes.findAll({
     attributes: ['id', 'name']
@@ -12,15 +16,25 @@ const readAllCompanyTypes = async () => {
   return response;
 }
 
+/**
+ *  Get a company type by id
+ * @param {Integer} id Gets the company type id 
+ * @returns {Object} returns the company type found
+ */
 const readCompanyTypesById = async (id) => {
   const response = await CompanyTypes.findOne({
     where: { id },
-    attributes: ['id','name' ]
+    attributes: ['id', 'name']
   })
 
   return response;
 }
 
+/**
+ * Create a company type
+ * @param {Object} data Gets data to create a company type
+ * @returns {object} return the company type created
+ */
 const createCompanyType = async (data) => {
   const response = await CompanyTypes.create({
     ...data,
@@ -30,17 +44,28 @@ const createCompanyType = async (data) => {
   return response;
 }
 
+/**
+ * Update a company type
+ * @param {Integer} companyTypeId Get the company type id 
+ * @param {Object} data Gets data to update a company type
+ * @returns {Object} return response
+ */
 const updateCompanyType = async (companyTypeId, data) => {
   const { id, ...restOfData } = data;
 
   const response = await CompanyTypes.update(
     restOfData,
-    {where: { id:companyTypeId }}
+    { where: { id: companyTypeId } }
   )
 
   return response;
 }
 
+/**
+ * Delete a company type
+ * @param {Integer} id Gets the company id 
+ * @returns {Object} return response
+ */
 const deleteCompanyType = async (id) => {
   const response = await CompanyTypes.destroy({
     where: { id }
